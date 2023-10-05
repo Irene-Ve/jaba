e46308a71df66f8012e731ca055657f79de68c86
Irene Veglio <ievo@gft.com>
2023-10-05 11:39:18 +0200

    modifiche sui nomi dati in args

--- src/main/java/com/example/jaba/m1/s2/Simple.java
+++ src/main/java/com/example/jaba/m1/s2/Simple.java
@@ -9,8 +9,9 @@ package com.example.jaba.m1.s2;
 // import java.lang.System;
 
 /**
- * A Java "Hello world"
- * 
+ * A Java "Hello world"  
+ * this is a javadoc comment!!
+ * this comment will be extract and used for documentation
  * @author Emanuele Galli
  */
 public class Simple {
@@ -22,10 +23,19 @@ public class Simple {
     public static void main(String[] args) {
         // output a string to console
         System.out.print("Hello ");
-
+    	//String name = args[0];
+    	
         // output is based on input arguments
         if (args.length == 1) {
             System.out.println(args[0]);
+        } else if (args.length == 2){
+        	  System.out.println(args[0] + " & " + args[1]);
+        } else {
+        	System.out.println("...stranger");
         }
+        
+        // example execution from Terminal 
+        //C:\Users\IEVO\git\jaba\src\main\java\com\example\jaba\m1>java s2/Simple.java "Silvia Maria" Giovanni
+        
     }
 }
